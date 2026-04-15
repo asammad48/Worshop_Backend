4a8a86f9d8dbf1700de738c98227429bb68a7c6e
Abdul Sammad <asammad48@gmail.com>
2026-04-15 21:17:25 +0500

    Fix DriverQueryRequest not to inherit sealed PageRequest

--- src/Application/DTOs/Drivers/DriverQueryRequest.cs
+++ src/Application/DTOs/Drivers/DriverQueryRequest.cs
@@ -1,8 +1,11 @@
-using Application.Pagination;
-
 namespace Application.DTOs.Drivers;
 
-public sealed class DriverQueryRequest : PageRequest
+public sealed class DriverQueryRequest
 {
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 20;
+    public string? Search { get; set; }
+    public string? SortBy { get; set; }
+    public string SortDirection { get; set; } = "asc";
     public Guid? CustomerId { get; set; }
 }
